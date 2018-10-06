758aeda81870b4b5314af367d8ae0933fa233a67
Bella DeCocco <decoccoisabella@gmail.com>
2018-10-06 19:27:42 -0400

    fixed navbar for various screen sizes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,15 +11,15 @@ const Navbar = () => (
         <div className="navbar-brand">
           <Link to="/" className="navbar-item logo">
             <figure className="image">
-              <img src={logo} alt="bella" style={{ width: '400px', }} />
+              <img src={logo} alt="bella" style={{ width: '160%', }} />
             </figure>
           </Link>
         </div>
       </div>
     </div>
     <div >
-      <div className="secondNav">        
-      <Link className="navbar-item" to="/blog/coding">
+      <div className="secondNav">
+        <Link className="navbar-item" to="/blog/coding">
           Code
         </Link>
         <Link className="navbar-item" to="/blog/webdesign">
@@ -35,29 +35,28 @@ const Navbar = () => (
           Summary
         </Link>
         <div className="navbar-end">
-          <a
-            className="navbar-item"
-            href="https://github.com/belladecocco"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span className="icon">
-              <img src={github} alt="Github" />
-            </span>
-          </a>
-          <a
-            className="navbar-item"
-            href="https://www.linkedin.com/in/isabella-decocco"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            <span className="icon">
-              <img src={linkedin} alt="LinkedIn" />
-            </span>
-            </a>
-        </div>
+        <a
+          className="navbar-item"
+          href="https://github.com/belladecocco"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className="icon">
+            <img src={github} alt="Github" />
+          </span>
+        </a>
+        <a
+          className="navbar-item"
+          href="https://www.linkedin.com/in/isabella-decocco"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className="icon">
+            <img src={linkedin} alt="LinkedIn" />
+          </span>
+        </a></div>
       </div>
-      </div>
+    </div>
   </nav>
 )
 
